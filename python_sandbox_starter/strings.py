76d41f2c5d98ab5b569e94a12323ce33ffc5300b
cpscott1 <thacam18gmail.com>
2020-01-26 01:32:00 -0600

    inital commit

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,5 +1,11 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Cam'
+age = 29
+
+# Concatenate
+print('Hello, my name is ' + name)
+
 
 # String Formatting
 
